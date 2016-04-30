ad84712bccaec304cce65adc69229eb0c71e8e7d
Bram Borggreve <borggreve@gmail.com>
2016-04-30 14:24:32 -0500

    Add user.info method which gives a summary of assigned/unassigned roles

--- common/models/user.js
+++ common/models/user.js
@@ -1,9 +1,11 @@
-module.exports = function (user) {
+var utils = require('loopback-datasource-juggler/lib/utils')
+
+module.exports = function(user) {
 
   // Set the username to the users email address by default.
-  user.observe('before save', function setDefaultUsername(ctx, next) {
+  user.observe('before save', function setDefaultUsername (ctx, next) {
     if (ctx.instance) {
-      if(ctx.isNewInstance) {
+      if (ctx.isNewInstance) {
         ctx.instance.username = ctx.instance.email;
       }
       ctx.instance.status = 'created';
@@ -11,5 +13,71 @@ module.exports = function (user) {
     }
     next();
   });
+
+  /**
+   * Method that retrieves all the roles in the system
+   * @param {Function} cb Callback function
+   * @returns boolean
+   */
+  user.getAllRoleNames = function getAllRoleNames(cb) {
+    cb = cb || utils.createPromiseCallback();
+
+    user.app.models.Role
+      .find()
+      .then(function(roles) {
+        return roles.map(function(role) {
+          return role.name;
+        })
+      })
+      .then(function(res) {
+        return cb(null, res)
+      })
+      .catch(cb)
+
+    return cb.promise
+  }
+
+  /**
+   * Get the roles for this user.
+   * @param {Function} [cb] Callback function.
+   * @returns {Object} A map of all roles of this user
+   */
+  user.prototype.info = function info(cb) {
+    cb = cb || utils.createPromiseCallback()
+
+    var result = {
+      user: this,
+      roles: {}
+    }
+    var allRoleNames = []
+    var userRoleNames = []
+
+    this.roles.getAsync()
+      .then(function(userRoles) {
+        return userRoles.map(function(userRole) {
+          return userRole.name
+        })
+      })
+      .then(function(res) {
+        userRoleNames = res
+        return user.getAllRoleNames()
+      })
+      .then(function(res) {
+        allRoleNames = res
+        result.roles = {
+          assigned: allRoleNames.filter(function(name) {
+            return userRoleNames.indexOf(name) !== -1;
+          }),
+          unassigned: allRoleNames.filter(function(name) {
+            return userRoleNames.indexOf(name) === -1;
+          })
+        }
+      })
+      .then(function() {
+        cb(null, result)
+      })
+      .catch(cb)
+    return cb.promise
+  }
 
 };
