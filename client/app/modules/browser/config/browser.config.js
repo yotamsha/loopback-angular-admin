dca702b2eb1fa3e62880d68061a6675290d5f065
yotam shalev <yotamsha@gmail.com>
2016-06-26 17:42:38 +0300

    separated the views to admin and public zones. - removed duplicated folders

--- client/app/modules/browser/config/browser.config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-  angular
-    .module('com.module.browser')
-    .run(function ($rootScope, Event, gettextCatalog) {
-      $rootScope.addMenu(gettextCatalog.getString('Browser'), 'app.admin.browser.models', 'fa-globe');
-    });
-
-})();
